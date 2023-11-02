950441b82c64bff86a89614f6c1617a600613b52
Guillermo Fernando Farfan Romero <75868855+Fernandofarfan@users.noreply.github.com>
2023-11-01 22:17:30 -0300

    Add files via upload

---
+++ Parcial-GuillermoFernandoFarfan/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import Card from './components/Card';
+import Form from './components/form';
+
+function App() {
+  //Aqui deberias agregar los estados y los handlers para los inputs
+  
+  // ----- VER VIDEO 1:50:03 ---------------//
+  //Falta
+  // Hacer validaciones
+  
+  const [informacion, setInformacion] = useState({}); {/**useState setea un objeto*/}
+  const [isMensajeError, setMensajeError] = useState(); {/**useState setea un objeto*/}
+  
+  // funcion dentro el padre "App" que se le pasa como "props" al hijo "Form"
+  // las props son de solo lectura, el hijo no puede modificarlas
+  const onSubmit = (data) => {
+    if (data.nombre.length >= 3 
+      && data.animal.length >= 6){
+        // si cumple la validacion, actualiza el estado de la "informacion"
+        console.log("data si: ", data);
+        setInformacion(data);
+        setMensajeError("false")
+      }else{
+        setMensajeError("true")
+        console.log("data no: ", data);
+
+    }
+  }
+  
+  return (
+    <div className="App">
+      <Form 
+        onSubmit={onSubmit} 
+        titulo="Animales"
+        isMensajeError={isMensajeError}
+        informacion={informacion}
+      />
+      <br />
+      <Card
+        isMensajeError={isMensajeError}
+        informacion={informacion}
+        />
+      <br />
+      <h5>Guillermo Fernando Farfan</h5>
+    </div>
+  );
+}
+
+export default App;
